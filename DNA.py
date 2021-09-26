8e44d24e387d1d2c3c273803daea2ee4be29ea2f
Andresdbp <71858994+Andresdbp@users.noreply.github.com>
2021-09-25 20:30:54 -0500

    Delete DNA.py

--- DNA.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-counter = Counter()
-seq = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
-for nt in seq:
-    counter[nt] += 1
-print(counter)
